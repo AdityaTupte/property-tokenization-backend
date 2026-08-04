235fa689e4027156a5c33094ba4edc13fdbb4452
Aditya Tupte <adityatupte2@gmail.com>
2026-08-05 04:14:40 +0530

    feat: refactor handleAddTrustee to improve trustee registry update logic and remove unused authority variable

--- src/controllers/solanaProgram.controller.ts/propertySystemCreation.controller/addTrustee.controller.ts
+++ src/controllers/solanaProgram.controller.ts/propertySystemCreation.controller/addTrustee.controller.ts
@@ -11,10 +11,7 @@ export const handleAddTrustee = async(
     instruction:Instructions
 ) => {
 
-
     const propertySystemAddress = address(message.accountKeys[instruction.accounts[1]!]!);
-
-    const authority =  address(message.accountKeys[instruction.accounts[0]!]!);
 
     const trustee_registry =  address(message.accountKeys[instruction.accounts[2]!]!);
 
@@ -25,10 +22,6 @@ export const handleAddTrustee = async(
         trustee_registry
       ) as PdaTypes.trusteeRegistry
     
-
-    // check that the trusteeRegistry is created or not 
-    // if not throw error 
-    // add to the trustee table
     
     const trusteeRegistiyAvailable = await prisma.trusteeRegistry.findFirst({
         where:{
@@ -41,20 +34,37 @@ export const handleAddTrustee = async(
 
 if(!trusteeRegistiyAvailable) throw new ApiError(404,"trusteeRegistry realted to the property System Not found");
 
-     await prisma.$transaction(async(tx) =>{
 
+    let hasTrusteeCountChanged = (trusteeRegistiyAvailable.current_number_of_trustees === trusteeRegistryaccount.currentNumberOfTrustees )
+
+    let hasTotalSalaryAllocatedChanged = (trusteeRegistiyAvailable.total_salary_allocated === BigInt(trusteeRegistryaccount.totalSalaryAllocated.toString()))
+
+    let hasVoteThresholdChanged = (trusteeRegistiyAvailable.vote_threshold === trusteeRegistryaccount.voteThreshold)
+    
+    let hasClaimDeadlineChanged = false ;
+
+    if(trusteeRegistryaccount.claimDeadlineTs.toNumber() !== 0){
+    
+   hasClaimDeadlineChanged  = (trusteeRegistiyAvailable.claim_deadline_ts ===new Date(trusteeRegistryaccount.claimDeadlineTs.toNumber() * 1000))
+
+}
+
+     await prisma.$transaction(async(tx) =>{
+            
         await tx.trusteeRegistry.update({
             where:{
                 trustee_registry_pubkey :trustee_registry,
             },
             data:{
-                current_number_of_trustees:trusteeRegistiyAvailable.current_number_of_trustees,
-                total_salary_allocated:trusteeRegistiyAvailable.total_salary_allocated,
-                claim_deadline_ts:trusteeRegistiyAvailable.claim_deadline_ts,
-                vote_threshold:trusteeRegistiyAvailable.vote_threshold,
+                current_number_of_trustees:hasTrusteeCountChanged? trusteeRegistryaccount.currentNumberOfTrustees:undefined,
+                total_salary_allocated:hasTotalSalaryAllocatedChanged?BigInt(trusteeRegistryaccount.totalSalaryAllocated.toString()):undefined,
+                claim_deadline_ts: trusteeRegistryaccount.claimDeadlineTs.toNumber() !== 0
+            ? new Date(trusteeRegistryaccount.claimDeadlineTs.toNumber() * 1000)
+            : null,
+                vote_threshold:hasVoteThresholdChanged?trusteeRegistryaccount.voteThreshold:undefined,
             }
         })
-
+        
 
         await tx.trustees.create({
             data:{
